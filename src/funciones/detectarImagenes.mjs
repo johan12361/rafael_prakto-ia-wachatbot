0e8a43a951ca48580621d0a75ba488c88e335372
Johan s <48106430+johan12361@users.noreply.github.com>
2025-03-03 23:13:47 -0500

    Act dep

--- src/funciones/detectarImagenes.mjs
+++ src/funciones/detectarImagenes.mjs
@@ -2,7 +2,6 @@ const imageRegex = /(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg|webp))/gi
 
 //TT EXTRAER IMAGENES DE RESPUESTA
 export function DetectarImagenes(text) {
-  const imageRegex = /(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg|webp))/gi
   const urls = text.match(imageRegex)
   return urls || null // Devuelve un array con las URLs o un NULL si no hay coincidencias
 }
